8af3134d7063805e9181a49ed452116ec148e05b
Ivaylo Kostov <kazanlak6@gmail.com>
2023-02-23 22:24:42 +0200

    added Open AI

---
+++ zodiac/app/Http/Controllers/CronController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpClient\Psr18Client;
+use Tectalic\OpenAi\Authentication;
+use Tectalic\OpenAi\Client;
+use Tectalic\OpenAi\Manager;
+use Carbon\Carbon;
+
+class CronController extends Controller
+{
+ 
+    function store($sign) { 
+         
+        $signs = config('constants');
+        $text_work = $this->get_text('работен', $signs[$sign]);
+        // $text_love = $this->get_text('любовен', $signs[$sign]);
+        // $text_health = $this->get_text('здравен', $signs[$sign]);
+        $date = Carbon::now()->format('Y-m-d');
+        dd($text_work, $text_love, $text_health);
+    }
+
+    function get_text($type, $sign) { 
+ 
+        $templeate = 'Напиши {type} хороскоп за {sign} на български език';
+        $prompt = str_replace(['{type}', '{sign}'], [$type, $sign], $templeate);
+        try {
+            $openaiClient = \Tectalic\OpenAi\Manager::build(new \GuzzleHttp\Client(), new \Tectalic\OpenAi\Authentication(getenv('OPENAI_API_KEY')));
+            $response = $openaiClient->completions()->create(
+                new \Tectalic\OpenAi\Models\Completions\CreateRequest([
+                    'model'  => 'text-davinci-003',
+                    'prompt' => $prompt,
+                ])
+            )->toModel();
+        } catch (ClientException $e) {
+            // Error response received. Retrieve the HTTP response code and response body.
+            $responseBody = $handler->toArray();
+            $responseCode = $handler->getResponse()->getStatusCode();
+            // Handle the error...
+        }
+    
+       echo $response->choices[0]->text;
+
+ 
+    }
+}
--- zodiac/routes/web.php
+++ zodiac/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Zodiac;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\CronController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('zodiac/{zodiac}/date/{date}', [Zodiac::class, 'single'])->name('single');
 
 Route::get('/page/{page}', [PagesController::class, 'show'])->name('single.page');
+Route::get('/cron/{sign}', [CronController::class, 'store'])->name('store.sign');
 
 
